dbdc47608729dcfca0d4ce848ec387e7bec41c38
Cornelis Heuperman <cornelis.heuperman@rezonence.com>
2019-01-15 14:04:52 +0000

    Implement basic counter and buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'incremental';
+  counter = 0;
+  incrementBy = 0;
+  cost = {
+    first: 0,
+    second: 20,
+    third: 400,
+    fourth: 8000,
+    fifth: 160000
+  };
+
+  ngOnInit() {
+    this.increment();
+  }
+
+  increment(): void {
+    setInterval(() => this.counter += (this.incrementBy / 10), 100);
+  }
+
+  makePurchase(value: number, cost: number) {
+    this.increaseIncrement(value);
+    this.subtractFromBalance(cost);
+    this.increaseCost(cost);
+  }
+
+increaseIncrement(value: number): void {
+  this.incrementBy += value;
+}
+
+  subtractFromBalance(cost: number): void {
+    this.counter -= cost;
+  }
+
+  increaseCost(cost: number) {
+    const stageToIncrease = Object.keys(this.cost).find(key => this.cost[key] === cost);
+    this.cost[stageToIncrease] = this.cost[stageToIncrease] * 1.3;
+  }
 }
