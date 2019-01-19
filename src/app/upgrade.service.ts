43a82e3ce22dd1e4867752bba38bad4e2b9515f1
Cornelis Heuperman <cornelis.heuperman@rezonence.com>
2019-01-19 17:34:12 +0000

    Change list layout to material design cards

--- src/app/upgrade.service.ts
+++ src/app/upgrade.service.ts
@@ -9,16 +9,16 @@ export class UpgradeService {
   constructor() { }
 
   upgrades = [
-    {title: 'double first', target: 'first', multiplier: 2, price: 10, requiredLevel: 10},
-    {title: 'double second', target: 'second', multiplier: 2, price: 200, requiredLevel: 10},
-    {title: 'double third', target: 'third', multiplier: 2, price: 4000, requiredLevel: 10},
-    {title: 'double fourth', target: 'fourth', multiplier: 2, price: 80000, requiredLevel: 10},
-    {title: 'double fifth', target: 'fifth', multiplier: 2, price: 1600000, requiredLevel: 10},
-    {title: 'double first', target: 'first', multiplier: 2, price: 1000, requiredLevel: 25},
-    {title: 'double second', target: 'second', multiplier: 2, price: 2E4, requiredLevel: 25},
-    {title: 'double third', target: 'third', multiplier: 2, price: 4E5, requiredLevel: 25},
-    {title: 'double fourth', target: 'fourth', multiplier: 2, price: 8E6, requiredLevel: 25},
-    {title: 'double fifth', target: 'fifth', multiplier: 2, price: 16E7, requiredLevel: 25}
+    {title: 'double first', target: 'first', multiplier: 2, price: 50, requiredLevel: 10},
+    {title: 'double second', target: 'second', multiplier: 2, price: 1000, requiredLevel: 10},
+    {title: 'double third', target: 'third', multiplier: 2, price: 2E4, requiredLevel: 10},
+    {title: 'double fourth', target: 'fourth', multiplier: 2, price: 4E5, requiredLevel: 10},
+    {title: 'double fifth', target: 'fifth', multiplier: 2, price: 8E6, requiredLevel: 10},
+    {title: 'double first', target: 'first', multiplier: 2, price: 5000, requiredLevel: 25},
+    {title: 'double second', target: 'second', multiplier: 2, price: 1E5, requiredLevel: 25},
+    {title: 'double third', target: 'third', multiplier: 2, price: 2E6, requiredLevel: 25},
+    {title: 'double fourth', target: 'fourth', multiplier: 2, price: 4E7, requiredLevel: 25},
+    {title: 'double fifth', target: 'fifth', multiplier: 2, price: 8E8, requiredLevel: 25}
   ];
 
   getUpgrades(): Upgrade[] {
