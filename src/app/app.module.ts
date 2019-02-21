c2b6bfe7dff5c48d92fe5c2b1228f099eecb77c1
Cornelis Heuperman <cornelis.heuperman@rezonence.com>
2019-02-21 10:52:59 +0000

    Add basic tooltips to side hustle buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {
   MatGridListModule,
   MatListModule,
   MatProgressBarModule,
+  MatTooltipModule,
   MatToolbarModule
 } from '@angular/material';
 import { SelfCareListComponent } from './self-care-list/self-care-list.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { FactoriesListComponent } from './factories-list/factories-list.component';
 import { UpgradesListComponent } from './upgrades-list/upgrades-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,16 +22,18 @@ import { UpgradesListComponent } from './upgrades-list/upgrades-list.component';
     SelfCareListComponent,
     TitleBarComponent,
     FactoriesListComponent,
-    UpgradesListComponent
+    UpgradesListComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
     MatGridListModule,
     MatListModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatTooltipModule
   ],
   providers: [
   ],
