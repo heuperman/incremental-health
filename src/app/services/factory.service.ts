6da7e00a5c0af8ded9ac48bf14045f823728cae3
Cornelis Heuperman <cornelis.heuperman@rezonence.com>
2019-04-25 10:37:37 +0100

    Update names and descriptions of upgrades

--- src/app/services/factory.service.ts
+++ src/app/services/factory.service.ts
@@ -12,8 +12,8 @@ export class FactoryService {
     {title: 'dog walking', baseProduction: 1, requiredStressReduction: 10},
     {title: 'babysitting', baseProduction: 10, requiredStressReduction: 100},
     {title: 'take-away delivery', baseProduction: 100, requiredStressReduction: 1000},
-    {title: 'uber driving', baseProduction: 1000, requiredStressReduction: 1E5},
-    {title: 'home baking', baseProduction: 1E4, requiredStressReduction: 1E6},
+    {title: 'uber driving', baseProduction: 1000, requiredStressReduction: 1E4},
+    {title: 'home baking', baseProduction: 1E4, requiredStressReduction: 1E5},
   ];
 
   getFactories(): Factory[] {
--- src/app/services/upgrade.service.ts
+++ src/app/services/upgrade.service.ts
@@ -9,13 +9,13 @@ export class UpgradeService {
   constructor() { }
 
   upgrades: Upgrade[] = [
-    {title: 'buy a kettle', description: 'unlock self-care level 2', id: 1, price: 10, requiredFunds: 5},
-    {title: 'buy a bathtub', description: 'unlock self-care level 3', id: 2, price: 100, requiredFunds: 50},
-    {title: 'hire  a massage therapist', description: 'unlock self-care level 4', id: 3, price: 1000, requiredFunds: 500},
-    {title: 'sign up for the local spa', description: 'unlock self-care level 5', id: 4, price: 1E5, requiredFunds: 5000},
+    {title: 'tea set', description: 'get all you need for a nice cup of tea', id: 1, price: 10, requiredFunds: 5},
+    {title: 'bathtub', description: 'have a freestanding bathtub installed', id: 2, price: 100, requiredFunds: 50},
+    {title: 'massage therapist', description: 'hire strong hands to kneed out the tension', id: 3, price: 1000, requiredFunds: 500},
+    {title: 'spa membership', description: 'sign up for your local pamper palace', id: 4, price: 1E4, requiredFunds: 5000},
     {title: 'more leashes', description: 'double income from dog walking', id: 5, target: 0, price: 50, requiredFunds: 25},
-    {title: 'target richer parents', description: 'double income from babysitting', id: 6 , target: 1, price: 500, requiredFunds: 250},
-    {title: 'get an e-bike', description: 'double income from take-away delivery', id: 7, target: 2, price: 5000, requiredFunds: 2500},
+    {title: 'twins only policy', description: 'double income from babysitting', id: 6 , target: 1, price: 500, requiredFunds: 250},
+    {title: 'upgrade to e-bike', description: 'double income from take-away delivery', id: 7, target: 2, price: 5000, requiredFunds: 2500},
     {title: 'drive a luxury car', description: 'double income from uber driving', id: 8, target: 3, price: 5E4, requiredFunds: 25000},
     {title: 'install twin ovens', description: 'double income from home baking', id: 9, target: 4, price: 5E5, requiredFunds: 25E4}
   ];
