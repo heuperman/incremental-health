e67cd0799df44048d18e01690033c282466a419a
Cornelis Heuperman <cheuperman@gmail.com>
2019-04-27 12:38:09 +0100

    Correct upgrade IDs

--- src/app/services/upgrade.service.ts
+++ src/app/services/upgrade.service.ts
@@ -14,12 +14,12 @@ export class UpgradeService {
     {title: 'massage therapist', description: 'hire strong hands to kneed out the tension', id: 3, price: 1000, requiredFunds: 500},
     {title: 'pet', description: 'adopt a lovable creature', id: 4, price: 1E4, requiredFunds: 5000},
     {title: 'spa membership', description: 'sign up for your local pamper palace', id: 5, price: 1E5, requiredFunds: 5E4},
-    {title: 'more leashes', description: 'double income from dog walking', id: 5, target: 0, price: 50, requiredFunds: 25},
-    {title: 'twins only policy', description: 'double income from babysitting', id: 6 , target: 1, price: 500, requiredFunds: 250},
-    {title: 'electric bike', description: 'double income from take-away delivery', id: 7, target: 2, price: 5000, requiredFunds: 2500},
-    {title: 'dual ovens', description: 'double income from home baking', id: 8, target: 3, price: 5E4, requiredFunds: 25000},
-    {title: 'trading bots', description: 'double income from crypto trading', id: 9, target: 4, price: 5E5, requiredFunds: 25E4},
-    {title: 'material goods', description: 'buy all the shiny things you ever wanted', id: 10, price: 5E6, requiredFunds: 25E5}
+    {title: 'more leashes', description: 'double income from dog walking', id: 6, target: 0, price: 50, requiredFunds: 25},
+    {title: 'twins only policy', description: 'double income from babysitting', id: 7 , target: 1, price: 500, requiredFunds: 250},
+    {title: 'electric bike', description: 'double income from take-away delivery', id: 8, target: 2, price: 5000, requiredFunds: 2500},
+    {title: 'dual ovens', description: 'double income from home baking', id: 9, target: 3, price: 5E4, requiredFunds: 25000},
+    {title: 'trading bots', description: 'double income from crypto trading', id: 10, target: 4, price: 5E5, requiredFunds: 25E4},
+    {title: 'material goods', description: 'buy all the shiny things you ever wanted', id: 11, price: 5E6, requiredFunds: 25E5}
   ];
   availableUpgrades: Upgrade[] = [];
 
